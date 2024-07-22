33192ea8a783c2abad7eea3ad2dda75194927d82
John Larson <jl@buildmidwestern.com>
2024-07-21 22:07:47 -0500

    feat: udpated home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,6 @@
     <div class="flex flex-row justify-center items-center gap-6">
         <button class="btn btn-primary btn-xs sm:btn-sm md:btn-md lg:btn-lg">Start Quiz</button>
         <button class="btn btn-primary btn-xs sm:btn-sm md:btn-md lg:btn-lg">View High Scores</button>
-        <a class="btn btn-primary btn-xs sm:btn-sm md:btn-md lg:btn-lg" href="/login">Login</a>
+        <a class="btn btn-primary btn-xs sm:btn-sm md:btn-md lg:btn-lg" href="/dashboard">Dashboard</a>
     </div>
 </section>
