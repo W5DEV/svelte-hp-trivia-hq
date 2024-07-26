877cb138b0c6d2f8133d5532c8cae59be349c696
John Larson <jl@buildmidwestern.com>
2024-07-25 21:31:51 -0500

    feat: updated buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,8 @@
     <h1 class="text-xl">Harry Potter Trivia</h1>
     <p>Test your knowledge of Harry Potter with random multi-choice or multi-select questions!</p>
     <div class="flex flex-row flex-wrap items-center justify-center gap-6">
-        <a class="btn btn-primary btn-wide" href="/quiz-selection">Select Quiz</a>
-        <button class="btn btn-primary btn-wide">View High Scores</button>
-        <a class="btn btn-primary btn-wide" href="/dashboard">Dashboard</a>
+        <a class="btn btn-primary text-base-100 btn-wide" href="/quiz-selection">Select Quiz</a>
+        <button class="btn btn-primary text-base-100 btn-wide">View High Scores</button>
+        <a class="btn btn-primary text-base-100 btn-wide" href="/dashboard">Dashboard</a>
     </div>
 </section>
