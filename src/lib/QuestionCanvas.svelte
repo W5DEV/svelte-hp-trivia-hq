38e8bddd12f30f1adff8dcde0faf26f40417affe
John Larson <jl@buildmidwestern.com>
2024-07-25 20:43:10 -0500

    fix: added difficulty meter

--- src/lib/QuestionCanvas.svelte
+++ src/lib/QuestionCanvas.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { Question } from "../store";
     import parchment from "../lib/images/parchment-bg.svg";
+	import { question_origins } from "../sources";
 
     export let question: Question;
     export let onAnswer: (answer: string) => void;
@@ -43,10 +44,28 @@
         selectedAnswer = [];
         onAnswer(finalAnswer);
     }
+
+    function getOriginLink() {
+        const origin = question_origins.find((origin) => origin.name === question.question_origin);
+        return origin?.link;
+    }
+
+    const difficulty = question.difficulty ? Math.floor(question.difficulty/10) : 5;
 </script>
 
 <div class="flex items-center justify-center w-screen font-caveat">
     <div class="flex flex-col items-center justify-between w-screen max-w-4xl gap-6 px-10 md:px-28 py-24 min-h-[600px] h-content" style="background-image: url({parchment}); background-size: cover; background-position: center; background-repeat: no-repeat;">
+        <div class="flex flex-row items-center justify-center w-full gap-2 mb-10 -mt-10">
+            <p class="mr-1 text-2xl font-medium text-neutral">Difficulty:</p>
+            <span class={'text-2xl font-semi-bold text-neutral ' + (difficulty < 3 ? 'text-green-600' : difficulty > 7 ? 'text-red-600' : 'text-yellow-400')}>
+                {difficulty}/10
+            </span>
+            <span class={(difficulty < 3 ? 'text-green-600' : difficulty > 7 ? 'text-red-600' : 'text-yellow-400')}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke-width="1.5" stroke="none" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
+                </svg>
+            </span>
+        </div>
         <div class="flex flex-col items-center justify-center w-full gap-4 text-left">
             <h2 class="text-3xl font-semibold text-neutral">Question {questionNumber} of {totalQuestions}:</h2>
             <h2 class="w-full text-4xl font-semibold text-left text-neutral">{question.type === "multi-select" ? "Select all that apply: " : question.type === "true-false" ? "True or False: "  : ""}{question.question}</h2>
@@ -74,5 +93,12 @@
                 <button on:click={() => handleAnswer()} class="text-4xl btn btn-ghost btn-wide text-neutral hover:text-secondary hover:bg-transparent">Next</button>
             </div>
         {/if}
+        <div class="flex flex-row items-end justify-end w-full top-10 right-10">
+            <a href={getOriginLink()} class="text-sm font-medium tooltip tooltip-left text-info" data-tip={question.question_origin}>
+                <svg xmlns="http://www.w3.org/2000/svg" target="_blank" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+                </svg>
+            </a>
+        </div>
     </div>
 </div>
