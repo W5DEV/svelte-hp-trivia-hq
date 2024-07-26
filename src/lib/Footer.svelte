7c5ab688649e9d51ee2e59773b99ed607d3f73ef
John Larson <jl@buildmidwestern.com>
2024-07-26 02:14:13 -0500

    feat: updated footer font

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,3 +1,3 @@
-<footer class="text-primary flex flex-col justify-center items-center w-full py-2">
-    <a class="text-xs opacity-50" href="https://greatidea.dev/contact">&copy; 2024 GreatIdea.dev | All Rights Reserved</a>
+<footer class="flex flex-col items-center justify-center w-full py-2 text-primary">
+    <a class="text-base font-bold font-caveat" href="https://greatidea.dev/contact">&copy; 2024 GreatIdea.dev | All Rights Reserved</a>
 </footer>
