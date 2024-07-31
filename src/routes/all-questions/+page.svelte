fa01f7774c45c3c2a2bca1fbc558a8314ae18426
John Larson <jl@buildmidwestern.com>
2024-07-31 00:47:49 -0500

    feat: added filter for all sources questions

--- src/routes/all-questions/+page.svelte
+++ src/routes/all-questions/+page.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from "svelte";
 	import { token, current_question } from "../../store";
 	import { goto } from "$app/navigation";
+	import { question_origins } from "../../sources";
 
    type Question = {
         id: string;
@@ -24,6 +25,8 @@
     onMount(() => {
         getUser();
     });
+
+    const sources = question_origins.filter((origin) => origin.active === true);
 
     const newToken = $token;
 
@@ -80,6 +83,18 @@
             }
         }
     }
+    let filter: HTMLSelectElement | string;
+    let filteredQuestions = questions;
+
+    $: filteredQuestions = questions.filter((question) => {
+        if (filter === 'all') {
+            return true;
+        } else {
+            return question.question_origin === filter;
+        }
+    });
+    $: console.log(filteredQuestions);
+    $: console.log(filter);
 </script>
 <section class="flex items-center justify-center font-medium text-secondary">
     <div class="flex flex-col items-center justify-center flex-1 max-w-4xl gap-6">
@@ -87,69 +102,80 @@
             <div class="flex flex-row flex-wrap items-center justify-center gap-4">
                 <a href="/dashboard" class="btn btn-primary text-base-100 btn-wide">Dashboard</a>
                 <a href="/sources" class="btn btn-primary text-base-100 btn-wide">Sources</a>
+                <select class="select select-bordered select-primary select-sm" bind:value={filter}>
+                    <option value="all">All</option>
+                    {#each sources as source}
+                        <option value={source.name}>{source.name}</option>
+                    {/each}
+                </select>
             </div>
             <div class="w-full join join-vertical">
-                {#each questions as question}
-                    <div class="border collapse collapse-arrow join-item border-base-300">
-                        <input type="radio" name="my-accordion-4" />
-                            <div class={question.completed === "true" ? `text-xl font-medium collapse-title text-success` : `text-xl font-medium collapse-title text-error`}>{question.question}</div>
-                        <div class="flex flex-col gap-3 collapse-content">
-                            <div class="flex flex-col">
-                                <p class="text-tertiary">Answers:</p>
-                                {#each question.answers as answer, i}
-                                    <div class="flex flex-row items-center justify-start gap-1">
-                                        <span class="text-primary">{i + 1}. </span>
-                                        <span class="text-primary">{answer}</span>
-                                    </div>
-                                {/each}
-                            </div>
-                            <div class="flex flex-col">
-                                <p class="text-tertiary">Tags:</p>
-                                <div class="flex flex-row items-center justify-start gap-1">
-                                    {#each question.tags as tag}
-                                            <span class="text-primary">{tag},</span>
+                {#each filteredQuestions as question, i}
+                    <div class="flex flex-row items-start justify-start gap-2">
+                        {#if filter  && filter !== 'all'}
+                            <span class="w-10 -ml-10 text-xl">{i + 1}</span>
+                        {/if}
+                        <div class="border collapse collapse-arrow join-item border-base-300">
+                            <input type="radio" name="my-accordion-4" />
+                                <div class={question.completed === "true" ? `text-xl font-medium collapse-title text-success` : `text-xl font-medium collapse-title text-error`}>{question.question}</div>
+                            <div class="flex flex-col gap-3 collapse-content">
+                                <div class="flex flex-col">
+                                    <p class="text-tertiary">Answers:</p>
+                                    {#each question.answers as answer, i}
+                                        <div class="flex flex-row items-center justify-start gap-1">
+                                            <span class="text-primary">{i + 1}. </span>
+                                            <span class="text-primary">{answer}</span>
+                                        </div>
                                     {/each}
                                 </div>
-                            </div>
-                            <div class="flex flex-col">
-                                <p class="text-tertiary">Source:</p>
-                                <div class="flex flex-row items-center justify-start gap-1">
-                                    <span class="text-primary">{question.source}</span>
+                                <div class="flex flex-col">
+                                    <p class="text-tertiary">Tags:</p>
+                                    <div class="flex flex-row items-center justify-start gap-1">
+                                        {#each question.tags as tag}
+                                                <span class="text-primary">{tag},</span>
+                                        {/each}
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="flex flex-col">
-                                <p class="text-tertiary">Question Origin:</p>
-                                <div class="flex flex-row items-center justify-start gap-1">
-                                    <span class="text-primary">{question.question_origin}</span>
+                                <div class="flex flex-col">
+                                    <p class="text-tertiary">Source:</p>
+                                    <div class="flex flex-row items-center justify-start gap-1">
+                                        <span class="text-primary">{question.source}</span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="flex flex-col">
-                                <p class="text-tertiary">Correct Answer:</p>
-                                <div class="flex flex-row items-center justify-start gap-1">
-                                    <span class="text-primary">{question.correct_answer}</span>
+                                <div class="flex flex-col">
+                                    <p class="text-tertiary">Question Origin:</p>
+                                    <div class="flex flex-row items-center justify-start gap-1">
+                                        <span class="text-primary">{question.question_origin}</span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="flex flex-row gap-4">
-                                <p class="text-tertiary">Difficulty:</p>
-                                <div class="flex flex-row items-center justify-start gap-1">
-                                    {#if question.difficulty}
-                                        <span class={question.difficulty < 26 ? `text-error` : question.difficulty < 76 ? `text-warning` : `text-success`}>{100 - question.difficulty}</span>
-                                    {:else}
-                                        <span class="text-neutral">No Data</span>
-                                    {/if}
+                                <div class="flex flex-col">
+                                    <p class="text-tertiary">Correct Answer:</p>
+                                    <div class="flex flex-row items-center justify-start gap-1">
+                                        <span class="text-primary">{question.correct_answer}</span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="flex flex-row gap-4">
-                                <p class="text-tertiary">Likes:</p>
-                                <div class="flex flex-row items-center justify-start gap-1">
-                                    {#if question.likes}
-                                        <span class="text-success">{question.likes}</span>
-                                    {:else}
-                                        <span class="text-error">0</span>
-                                    {/if}
+                                <div class="flex flex-row gap-4">
+                                    <p class="text-tertiary">Difficulty:</p>
+                                    <div class="flex flex-row items-center justify-start gap-1">
+                                        {#if question.difficulty}
+                                            <span class={question.difficulty < 26 ? `text-error` : question.difficulty < 76 ? `text-warning` : `text-success`}>{100 - question.difficulty}</span>
+                                        {:else}
+                                            <span class="text-neutral">No Data</span>
+                                        {/if}
+                                    </div>
                                 </div>
+                                <div class="flex flex-row gap-4">
+                                    <p class="text-tertiary">Likes:</p>
+                                    <div class="flex flex-row items-center justify-start gap-1">
+                                        {#if question.likes}
+                                            <span class="text-success">{question.likes}</span>
+                                        {:else}
+                                            <span class="text-error">0</span>
+                                        {/if}
+                                    </div>
+                                </div>
+                                <div class="flex flex-row items-center justify-end w-full"><button on:click={handleEditClick(question)} class="btn btn-primary btn-sm text-base-100">Edit</button></div>
                             </div>
-                            <div class="flex flex-row items-center justify-end w-full"><button on:click={handleEditClick(question)} class="btn btn-primary btn-sm text-base-100">Edit</button></div>
                         </div>
                     </div>
                 {/each}
