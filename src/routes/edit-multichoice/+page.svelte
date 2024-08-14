c24bd4d392d31cc595a594c45bbe01c3c4345c6a
John Larson <jl@buildmidwestern.com>
2024-08-14 10:36:09 -0500

    fix: fixed upload objects

--- src/routes/edit-multichoice/+page.svelte
+++ src/routes/edit-multichoice/+page.svelte
@@ -113,7 +113,8 @@
 			type: 'multi-choice',
 			correct_answer: $form.correct_answer.value,
             completed: $form.completed.value ? 'true' : 'false',
-            question_origin: $form.question_origin.value
+            question_origin: $form.question_origin.value,
+            topic: 'Harry Potter'
 		};
 		try {
 			const response = await fetch(`https://hp-api.greatidea.dev/api/questions/${question?.id}`, {
