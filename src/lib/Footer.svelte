fba5d7d442838119b935db7a08dad8b706c16a19
John Larson <jl@buildmidwestern.com>
2024-08-16 15:51:45 -0500

    feat: updated footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,3 +1,3 @@
 <footer class="flex flex-col items-center justify-center w-full py-2 text-primary">
-    <a class="text-base font-bold font-caveat" href="https://greatidea.dev/contact">&copy; 2024 GreatIdea.dev | All Rights Reserved</a>
+    <a class="text-base font-bold font-caveat" href="https://greatidea.dev/contact">&copy; {new Date().getFullYear()} GreatIdea.dev | All Rights Reserved</a>
 </footer>
